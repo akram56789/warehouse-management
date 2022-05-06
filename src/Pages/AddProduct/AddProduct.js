99c876a4cf216a1d5a11958f071fe421ad578daf
akram56789 <akramhossain56789@gmail>
2022-05-06 21:21:06 +0600

    addproduct form done

--- src/Pages/AddProduct/AddProduct.js
+++ src/Pages/AddProduct/AddProduct.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+
 
 const AddProduct = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
     return (
-        <div>
-            
-        </div>
+        <div className='w-50 mx-auto'>
+        <h2>Please add a service</h2>
+        <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+            <input className='mb-2' placeholder='name' {...register("name", { required: true, maxLength: 20 })} />
+            <textarea className='mb-2' placeholder='Description' {...register("description")} />
+            <input className='mb-2' placeholder='Price' type="number" {...register("price")} />
+            <input className='mb-2' placeholder='Photo Url' type="text" {...register("img")} />
+            <input className='w-50 mx-auto' type='submit' value="Add Service" />
+        </form>
+    </div>
     );
 };
 
